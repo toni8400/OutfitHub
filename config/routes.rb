07b611beb0fe68d675b6daa0e4e3a4ac94343504
Wagner Wieland <wagnerp5@hotmail.com>
2023-03-02 15:46:37 -0300

    router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :offers do
+    resources :bookings, only: [:index, :new, :create]
+  end
+  resources :bookings, only: [:show, :edit, :update]
 end
